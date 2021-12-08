8b7b1c047bba42f2d738169a6464b8bb18d045a1
Vinh Ly <lyvinh1023@gmail.com>
2021-12-08 17:59:08 +0700

    test 2

--- app/src/androidTest/java/com/example/samplehybridapp/HomeTests.java
+++ app/src/androidTest/java/com/example/samplehybridapp/HomeTests.java
@@ -4,7 +4,6 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
@@ -23,7 +22,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
@@ -31,31 +29,35 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
 @RunWith(AndroidJUnit4.class)
-@LargeTest
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class DemoWithinSourceTests {
+public class HomeTests {
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
-    @Before
-    public void setup() {
+//    @Test
+//    public void menuBarTest11() {
+//        onView(withId(R.id.navigation_dashboard)).perform(click());
+//        onView(withText("This is dashboard page")).check(matches(isDisplayed()));
+//        onView(withId(R.id.navigation_notifications)).perform(click());
+//        onView(withText("This is notifications page")).check(matches(isDisplayed()));
+//        onView(withId(R.id.navigation_login))
+//                .check(matches(isEnabled()))
+//                .perform(click());
+//    }
+
+    @Test
+    public void navigationBarTest() {
+        onView(withId(R.id.navigation_login))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.navigation_dashboard))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.navigation_notifications))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void loginWebViewTest() throws InterruptedException {
         onWebView(withId(R.id.home_webview)).forceJavascriptEnabled();
-    }
-
-    @Test
-    public void test1() {
-        onView(withId(R.id.navigation_dashboard)).perform(click());
-        onView(withText("This is dashboard page")).check(matches(isDisplayed()));
-        onView(withId(R.id.navigation_notifications)).perform(click());
-        onView(withText("This is notifications page")).check(matches(isDisplayed()));
-        onView(withId(R.id.navigation_home))
-                .check(matches(isEnabled()))
-                .perform(click());
-    }
-
-    @Test
-    public void test2() throws InterruptedException {
         onWebView()
                 .withElement(findElement(Locator.NAME, "username"))
                 .perform(webKeys("test"))
