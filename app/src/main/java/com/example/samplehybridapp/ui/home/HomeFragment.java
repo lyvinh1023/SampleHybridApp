3776682af9d89af80b1a0d7c31207ab77ca3127c
Vinh Ly <lyvinh1023@gmail.com>
2021-12-08 16:28:07 +0700

    first commit

---
+++ app/src/main/java/com/example/samplehybridapp/ui/home/HomeFragment.java
@@ -0,0 +1,50 @@
+package com.example.samplehybridapp.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.samplehybridapp.R;
+import com.example.samplehybridapp.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        WebView mWebView = (WebView) binding.homeWebview;
+        mWebView.loadUrl("https://www.stealmylogin.com/demo.html");
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
