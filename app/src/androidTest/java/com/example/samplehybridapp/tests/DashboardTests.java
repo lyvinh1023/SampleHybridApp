5195f3a0f387cb07a8ce9fb7208c02399f22ebc8
Vinh Ly <lyvinh1023@gmail.com>
2021-12-14 15:43:04 +0700

    Add AndroidJUnit4 runner into test classes

--- app/src/androidTest/java/com/example/samplehybridapp/tests/DashboardTests.java
+++ app/src/androidTest/java/com/example/samplehybridapp/tests/DashboardTests.java
@@ -1,8 +1,12 @@
 package com.example.samplehybridapp.tests;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import com.example.samplehybridapp.pages.DashboardPage;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class DashboardTests extends BaseTests {
 
     DashboardPage dashboardPage = new DashboardPage();
--- app/src/androidTest/java/com/example/samplehybridapp/tests/NotificationsTests.java
+++ app/src/androidTest/java/com/example/samplehybridapp/tests/NotificationsTests.java
@@ -1,8 +1,12 @@
 package com.example.samplehybridapp.tests;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import com.example.samplehybridapp.pages.NotificationsPage;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class NotificationsTests extends BaseTests {
 
     NotificationsPage notificationsPage = new NotificationsPage();
